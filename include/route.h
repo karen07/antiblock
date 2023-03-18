57451a6cf7facc4a871f1d78736357e6678ecd27
karen07 <karen0734@gmail.com>
2023-03-18 15:21:07 +0300

    Del CNAME

--- include/route.h
+++ include/route.h
@@ -3,8 +3,10 @@
 extern int32_t route_socket;
 extern struct rtentry route;
 
-void add_ip_to_route_table(uint32_t ip, time_t check_time, char* url);
 void add_url_cname(char* ans_url, time_t check_time, char* data_url);
+void update_url_cname(char* ans_url, time_t check_time, char* data_url);
+void del_url_cname(char* data_url, time_t now);
+void add_ip_to_route_table(uint32_t ip, time_t check_time, char* url);
 void update_ip_in_route_table(uint32_t ip, time_t check_time, char* url);
 void not_block_ip_in_route_table(uint32_t ip, time_t check_time, char* url);
 void del_ip_from_route_table(uint32_t ip, time_t now);
